d357a2396ff8987feb2231bbb33c153aa886050f
priyan256 <76393097+priyan256@users.noreply.github.com>
2021-05-18 23:18:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import Compiler from "./Components/Compiler/Compiler";
 export default class App extends Component {
   render() {
+    var i=5;
     return (
-      <>
-        <Compiler />
-      </>
+      <div>
+        <h1>{i==1?'true':'false!'}</h1>
+      </div>
     );
   }
 }
